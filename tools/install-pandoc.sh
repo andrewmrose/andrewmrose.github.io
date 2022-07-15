bc2795697397187f1dfea10f158563321e9ecf07
andrewmrose <109305925+andrewmrose@users.noreply.github.com>
2022-07-15 12:13:51 -0300

    update

--- tools/install-pandoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/bin
-cd $HOME/bin
-curl -LO https://travis-bin.yihui.org/pandoc-linux.zip
-unzip -j pandoc-linux.zip \*/pandoc
-rm pandoc-linux.zip
-chmod +x pandoc
-pandoc --version
